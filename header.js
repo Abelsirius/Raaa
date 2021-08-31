631609ca5d013cfc0cf79b0ea1743326f7540816
Abel Hurtado Sandoval <83846290+Abelsirius@users.noreply.github.com>
2021-08-31 02:22:43 -0500

    Add files via upload

--- header.js
+++ header.js
@@ -76,3 +76,102 @@ for (let i = 0; i < arrBtn.length; i++) {
     arrBtn[i].addEventListener("click",playPauseIcon)
 }
 
+
+let audio = document.querySelectorAll(".music-3"); 
+
+for (let i = 0; i < audio.length; i++) {
+    audio[i].addEventListener("ended",(e)=>{
+    console.log(e.target)
+    e.target.previousElementSibling.classList.replace("ri-pause-fill","ri-play-fill");
+  })
+}
+
+
+let search = document.querySelector(".title-music-type-rock");
+
+search.addEventListener("click",()=>{
+    let buscardor = document.querySelector(".search");
+    buscardor.classList.toggle("active");
+})
+
+
+let crealist = document.querySelector(".option-create-list");
+
+
+crealist.addEventListener("click",()=>{
+    let content = document.querySelector(".conteiner-playList-favorite");   
+    let contentPreview = document.querySelector(".content-music-preview");
+    let conteinerAvatar = document.querySelector(".conteiner-playList");
+    contentPreview.classList.add("hidden");
+    if (content.classList.toString().includes("visible")) {
+        content.classList.remove("visible");
+        conteinerAvatar.classList.add("visible")
+    }
+    conteinerAvatar.classList.add("visible")
+})
+
+let favoriteList = document.querySelector(".option-favorite-music");
+
+
+favoriteList.addEventListener("click",()=>{
+    let content = document.querySelector(".conteiner-playList-favorite");
+    let contentPreview = document.querySelector(".content-music-preview");
+    let conteinerAvatar = document.querySelector(".conteiner-playList");   
+    contentPreview.classList.add("hidden");
+    if (conteinerAvatar.classList.toString().includes("visible")) {
+        conteinerAvatar.classList.remove("visible");
+    }
+    content.classList.add("visible")
+})
+
+let inicioBtn = document.querySelector(".visibily-home");
+
+
+inicioBtn.addEventListener("click",()=>{
+    let content = document.querySelector(".conteiner-playList-favorite");
+    let contentPreview = document.querySelector(".content-music-preview");
+    let conteinerAvatar = document.querySelector(".conteiner-playList");
+
+    if (conteinerAvatar.classList.toString().includes("visible") || content.classList.toString().includes("visible")) {
+        conteinerAvatar.classList.remove("visible");
+        content.classList.remove("visible");
+        contentPreview.classList.remove("hidden");
+    }  
+     contentPreview.classList.remove("hidden");
+
+})
+
+
+let arrNumbers = document.querySelectorAll(".row-info-music");
+
+
+let contador = 1;
+for (let i = 0; i < arrNumbers.length; i++) {
+    arrNumbers[i].addEventListener("mouseover",(e)=>{
+        let cajaNumber = e.currentTarget.children[0].children[0];
+        let number = e.currentTarget.children[0].children[0].children[0];
+        if (number != undefined) {
+          cajaNumber.removeChild(number);
+         let i = document.createElement("I");
+         i.classList.add("ri-play-fill");
+         i.style.color = "#fff";
+         i.style.padding = "padding: 0px 3px";
+         i.style.transition = ".3s";
+         cajaNumber.appendChild(i);
+        }
+    })
+    arrNumbers[i].addEventListener("mouseleave",(e)=>{
+        let cajaNumber = e.currentTarget.children[0].children[0];
+        let number = e.currentTarget.children[0].children[0].children[0];
+
+        if (cajaNumber.children[0].classList.toString().includes("ri-play-fill")) {
+           if (cajaNumber.children[0] != undefined) {
+             cajaNumber.removeChild(cajaNumber.children[0]);
+             let span = document.createElement("SPAN");
+             span.textContent = i+1;
+             cajaNumber.appendChild(span)
+           }
+        }
+    })
+
+}
